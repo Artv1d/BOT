5602789eafd908e786f0164fdffd92742ce9027d
Artv1d <154266855+Artv1d@users.noreply.github.com>
2024-04-12 02:44:38 +0300

    Update takeToken.py

--- takeToken.py
+++ takeToken.py
@@ -1,12 +1,22 @@
 import telebot
 import config
 import random
+import sqlite3
 
 bot = telebot.TeleBot(config.TOKEN)
 
 def generate_unique_token():
-    while True:
-        token = random.randint(1, 2)
-        if token not in config.existing_tokens:
-            config.existing_tokens.add(token)
-            return token
+    existing_tokens = set()
+    conn = sqlite3.connect('shop.sql')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM tokens')
+    items = cur.fetchall()
+    # Получаем результат запроса
+    for el in items:
+        existing_tokens.add(el[5])
+    cur.close()
+    conn.close()
+    available_tokens = set(range(1, 5)) - existing_tokens
+    if not available_tokens:
+        return None
+    return (random.choice(list(available_tokens)))
